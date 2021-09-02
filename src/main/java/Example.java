eb80e6fcd3f84ea1969135469378699b4d984793
Mohan <divay.mohan@teradata.com>
2021-09-02 16:36:51 +0530

    My Pipeline

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,17 +1,14 @@
-
-import java.util.Arrays;
-import java.util.List;
-
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Create;
-import org.apache.beam.sdk.transforms.FlatMapElements;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.values.TypeDescriptors;
+import org.apache.beam.sdk.transforms.SimpleFunction;
 import org.apache.beam.sdk.transforms.Count;
 import org.apache.beam.sdk.values.KV;
+
 
 public class Example {
 
@@ -19,16 +16,40 @@ public class Example {
 		PipelineOptions pipelineOptions = PipelineOptionsFactory.create(); 
 		
 		Pipeline pipeline = Pipeline.create(pipelineOptions);
-		
 		pipeline.apply(TextIO.read().from("C:\\Users\\dm255078\\OneDrive - Teradata\\Documents\\Learnings\\word-count-beam\\input\\input.txt"))
-		.apply("ExtractWords",FlatMapElements.into(TypeDescriptors.strings()).via((String line) -> Arrays.asList(line.split("[^\\p{L}]+")) ))
-		.apply(Count.<String>perElement())
-		.apply("FormatResults",MapElements.into(TypeDescriptors.strings()).via((KV<String,Long> wordCount) -> wordCount.getKey()+": "+wordCount.getValue()))
-		.apply(TextIO.write().to("C:\\Users\\dm255078\\OneDrive - Teradata\\Documents\\Learnings\\word-count-beam\\output\\wordcount"));
+                .apply("ExtractWords", ParDo.of(new DoFn<String, String>() {
+
+					@ProcessElement
+                    public void processElement(ProcessContext c) {
+                        for (String word : c.element().split("[^\\p{L}]+")) {
+                            if (!word.isEmpty()) {
+                                c.output(word);
+                            }
+                        }
+                    }
+                }))
+                .apply(Count.<String>perElement())
+                .apply(ParDo.of(new DoFn<KV<String, Long>, KV<String, Long>>() {
+					@ProcessElement
+                    public void processElement(ProcessContext c){
+						
+                        KV<String, Long> element = c.element();
+                        if(element.getKey().length() <= 3) {
+                            c.output(element);
+                        }
+                    }
+                }))
+                .apply("FormatResults", MapElements.via(new SimpleFunction<KV<String,Long>, String>() {
+                    
+
+					@Override
+                    public String apply(KV<String, Long> input) {
+                        return input.getKey() +": " + input.getValue();
+                    }
+                }))
+                .apply(TextIO.write().to("C:\\Users\\dm255078\\OneDrive - Teradata\\Documents\\Learnings\\word-count-beam\\output\\wordcount"));
 		
-		pipeline.run().waitUntilFinish();
-		
-	
+                pipeline.run().waitUntilFinish();
 		
 
 	}
